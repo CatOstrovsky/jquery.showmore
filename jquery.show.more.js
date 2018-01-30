8854eb121037011778f97298e926a556670dfcc0
Konstantin <ska_live@mail.ru>
2018-01-30 18:01:02 +0300

    add wrapper before work

--- jquery.show.more.js
+++ jquery.show.more.js
@@ -3,18 +3,22 @@ jQuery.fn.extend({
 
         if(!limit) limit = 10;
 
+            console.log(this )
+
         $(this).each(function(i,e){
+            var wrap = $("<div>").append($(e).html());
+            $(e).html(wrap);
+            e = wrap;
 
             var maxHeightString = parseInt(limit),
             maxHeightPx = parseInt($(e).css("lineHeight"))*maxHeightString,
             height = $(e).outerHeight(false);
             $(e).data("original-height", $(e).outerHeight(true));
-
             if(height > maxHeightPx){
                 $(e).css({height:maxHeightPx,overflow:"hidden"});
                
 
-                var showMore = $("<a>").text("Показать полностью").attr({class:"showMorer"}).bind("click", function(){
+                var showMore = $("<a>").text("Читать дальше").attr({class:"showMorer"}).bind("click", function(){
                     
                     $(e).animate({
                         height: $(e).data("original-height")
